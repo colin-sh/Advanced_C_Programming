2e675a71583f229f75de57d3a95335e7dd8f518d
eachlaw <colin5496@naver.com>
2017-03-16 23:23:12 +0900

    add pointer training

--- Advanced_C_prog/Week3_Pointer.c
+++ Advanced_C_prog/Week3_Pointer.c
@@ -1,4 +1,49 @@
 #include <stdio.h>
+
+//수업중 실습6
+int main() {
+	int *p1 = NULL;
+	char *p2 = NULL;
+
+	printf("%p %p\n", p1, p2);
+	printf("%p %p\n", p1+1, p2+1);
+	printf("%p %p\n", p1+2, p2+2);
+
+	return 0;
+}
+
+/*
+//수업중 실습5
+int main() {
+	char car[5] = { 'H','e','l','l','o' }, *cp = car;
+	double dar[5] = { 1.1,2.2,3.3,4.4,5.5 }, *dp = dar;
+	printf("%p %c %c\n", cp, cp[0], *cp);
+	printf("%p %c %c\n", cp + 1, cp[1], *(cp + 1));
+	printf("%p %c %c\n", cp + 2, cp[2], *(cp + 2));
+	printf("\n");
+	printf("%p %f %f\n", dp, dp[0], *dp);
+	printf("%p %f %f\n", dp + 1, dp[1], *(dp + 1));
+	printf("%p %f %f\n", dp + 2, dp[2], *(dp + 2));
+
+	return 0;
+}
+*/
+/*
+//수업중 실습4
+int main() {
+	char car[5] = { 'H','e','l','l','o' };
+	double dar[5] = { 1.1,2.2,3.3,4.4,5.5 };
+	printf("%p %c %c\n", car, car[0], *car);
+	printf("%p %c %c\n", car+1, car[1], *(car+1));
+	printf("%p %c %c\n", car+2, car[2], *(car+2));
+	printf("\n");
+	printf("%p %f %f\n", dar, dar[0], *dar);
+	printf("%p %f %f\n", dar+1, dar[1], *(dar+1));
+	printf("%p %f %f\n", dar+2, dar[2], *(dar+2));
+
+	return 0;
+}
+*/
 /*
 int main() {
 	int num1, num2, *p = &num1;
